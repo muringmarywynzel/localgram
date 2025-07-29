36ac21c783294bd1313f96df82049e319bd10979
Mary Wynzel Muring <muringmarywynzel@gmail.com>
2025-07-29 13:23:01 +0800

    Initial project commit.

---
+++ LocalGram/LocalGram/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LocalGram
+//
+//  Created by Mary Wynzel Muring on 7/29/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
